08bacde0550e5a6f92915853aeb4169ebdfc20c7
daniel nava <danielnava901@hotmail.com>
2019-10-14 18:54:08 -0500

    commit

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -6,20 +6,41 @@ class UserController {
     async store({request}) {
         console.log("intentando registrar...");
         const {username, email, password} = request.all();
+        const user = null
+        try {
+            user = await User.create({
+                username,
+                email,
+                password
+            });
+            return this.login(...arguments);
+        }catch(e) {
+            console.log("[store error]", e);
+            return {
+                code: 401,
+                msg: "user exist"
+            }; 
+        }
+    
         
-        const user = await User.create({
-            username,
-            email,
-            password
-        });
-
-        return this.login(...arguments);
     }
 
     async login({request, auth}) {
         const {email, password} = request.all();
-        const token = await auth.attempt(email, password);
-        return token;
+        try {
+            let user = await auth.attempt(email, password);
+            console.log("user", user);
+            return {
+                code: 200,
+                user
+            };
+        }catch(e) {
+            return {
+                code: 401,
+                msg: "user not found"
+            }
+        }
+        
     }
 
     async get({ params, request }) {
